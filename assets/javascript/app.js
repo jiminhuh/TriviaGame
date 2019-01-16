b9741531f5f9ccc3442caaa947d52db29356472b
Jimin Huh <jiminhuh95@gmail.com>
2019-01-15 18:28:56 -0600

    added bootstrap and basic html and css, 50% completed

---
+++ assets/javascript/app.js
@@ -0,0 +1,8 @@
+//Click Start button to wipe game div and start quiz
+//Each question will be an object inside a game object
+//   - question:
+//   - answer: (this will be a number based on which radio button is pressed)
+//This will automatically start countdown from 30 secs.
+//If countdown reaches 0 secs, display "Time's Up" then move on to next question in 2 secs.
+//If player chooses wrong answer display "Wrong" then move on to next questions in 2 secs.
+//At the end, display # questions right, # questions wrong, and option to restart game. 
